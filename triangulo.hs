9658e9108a528e0b8aff6f07fad2b411c4f10912
Lucas Anand Mazotti Ferretti <82159593+lucasanand9@users.noreply.github.com>
2023-03-23 16:29:14 -0300

    Add files via upload

---
+++ triangulo.hs
@@ -0,0 +1,7 @@
+-- A funçao deve receber 3 valores. 
+-- Com esses valores, ela deve calcular
+-- se é possivel a existencia de um triangulo
+
+soma a b = a + b
+
+triangulo a b c = (soma a b) > c && (soma b c) > a && (soma c a) > c
